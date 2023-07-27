d62847f8fabb1bd96815ebaa9d26d7f94e13a4f6
Soundharya Gomathi S <114283701+Soundharya0903@users.noreply.github.com>
2023-07-27 14:18:17 +0530

    Add files via upload

---
+++ logic.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.*;
+public class logic{
+public static void main(String[] args)
+{
+String str;
+int i;
+Scanner sc = new Scanner(System.in);
+System.out.println("enter the string:");
+ str=next();
+for(i=0;i<str.length();i++)
+{
+if(str.length() < 4)
+{
+ System.out.println("invalid");
+}
+else
+    System.out.println("valid");
+}}}
